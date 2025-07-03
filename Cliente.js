ef56a8cffda386c75fcb2e17f83d42ab0c1db06b
Angie Samaniego <angie@ejemplo.com>
2025-07-02 21:43:50 -0500

    Iniciando con git

---
+++ Cliente.js
@@ -0,0 +1,22 @@
+class Cliente {
+    constructor (nombre, cedula){
+        if (!nombre ||typeof nombre !== 'string'){
+            throw new Error ('Nombre invalido');
+        }
+        if (!cedula ||typeof cedula !== 'number'){ // agregar validación de la cedula (consultar)
+            throw new Error ('Cedula invalida');
+        }
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.cuenta = null; //cada cliente puede tener una cuenta
+    }
+
+    asignarCuenta(cuenta){
+        this.cuenta = cuenta; // estamos asignando una cuenta 
+    }
+
+    obtenerInformacion (){
+        return `El cliente ${this.nombre}, con cedula: ${this.cedula}, tiene n° cuenta: ${this.cuenta}`;
+    }
+}
+export default Cliente;
